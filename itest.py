b8bf29e503114eb760756aa819ebc544d0ec2edf
engineer <josephross004@gmail.com>
2024-07-26 15:26:17 -0700

    installation test

---
+++ itest.py
@@ -0,0 +1,14 @@
+import acoustic_survey
+print("acoustic_survey: passed")
+import hydrophone
+print("hydrophone: passed")
+import acoustic_file
+print("acoustic file: passed")
+import ssignal
+print("signal: passed")
+import units
+print("units: passed")
+import utils
+print("utils: passed")
+import plots
+print("plots: passed")
