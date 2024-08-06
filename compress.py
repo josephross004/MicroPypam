09972dad8dc0fa13f36006ca245552d06a394b98
josephross004 <josephross004@gmail.com>
2024-08-06 08:12:19 -0700

    fix compression - compat errors

--- compress.py
+++ compress.py
@@ -20,37 +20,20 @@ def compress(path,outputname):
             # Convert numpy array txt file to hexadecimal (COMPRESSION: 2:1)
             for i in A:
                 for j in i:
-                    if j>127 and j<177:
-                        #HACK: fix compatibility issues with utf-8
-                        s += "c2"+str(hex(int(j)+68))[2:]
+                    if j < 158 and j >= 127:
+                        s += "SPc2"+str(hex(int(j)+34))[2:]
+                    elif j < 222 and j >= 158:
+                        s += "SPc3"+str(hex(int(j)-30))[2:]
+                    elif j >= 222:
+                        s += "SPc4"+str(hex(int(j)-94))[2:]
                     else:
-                        s += str(hex(int(j)))[2:]
-            C.append(s)
-            for i in range(0,len(s)-1,2):
-                if s[i:i+2]=="c2":
-                    s = s[:i]+"SP"+s[i+2:]
+                        s += "SP"+str(hex(int(j)))[2:]
             hexes = s.split("SP")
             FLAG = False
-
+            hexes.pop(0)
             # decode hexadecmial, 2 numbers to 1 byte. (net COMPRESSION: 4:1)
-            for i in hexes:
-                if i=="":
-                     i = 'c2'    
-                if not FLAG:
-                    O.append(bytes.fromhex(i).decode("utf-8"))
-                    FLAG = True
-                else:
-                    try:
-                        O.append(bytes.fromhex("c2"+i).decode("utf-8"))
-                    except UnicodeDecodeError:
-                        t = (int(i[0:2],16))
-                        while t > 191:
-                            t -= 50
-                        while t < 128:
-                            t += 50
-                        t = str(hex(t))[2:]
-                        i = t + i[2:]
-                        O.append(bytes.fromhex("c3"+i).decode("utf-8"))
+            for i in hexes:   
+                O.append(bytes.fromhex(i).decode("utf-8"))
     with codecs.open(outputname,"w","utf-8-sig") as f:
         for i in O:
             f.write(str(i))
@@ -75,15 +58,19 @@ def decompress(path,output,pctls):
             decoded += "\n"
         hexrep = i.encode("utf-8").hex()
         if len(hexrep)==2:
+            # length is 2. the character is easily convertible.
             decoded += str(int(hexrep,16))+" "
         else:
-            #length is 4. the character either begins with c2 or c3.
-            #if it's c2, then subtract 68,
-            #if it's c3, then subtract 18.
-            if hexrep[1]=='3':
-                decoded += str(int(hexrep[2:],16)-18) + " "
+            # length is 4.
+            if hexrep[1]=='2':
+                # c2XX
+                decoded += str(int(hexrep[2:],16)-34) + " "
+            elif hexrep[1]=='3':
+                # c3XX
+                decoded += str(int(hexrep[2:],16)+30) + " "
             else:
-                decoded += str(int(hexrep[2:],16)-68) + " "
+                # c4XX
+                decoded += str(int(hexrep[2:],16)+94) + " "
         newline_flag += 1
     with open(output, 'w') as o:
         o.write(decoded)
